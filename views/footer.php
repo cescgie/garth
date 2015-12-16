81a286363fc453f1d07fc4012533cba94fadb82c
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-12-16 14:25:41 +0100

    Footer added

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,20 @@
+    <footer class="page-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="white-text">Footer Content</h5>
+            <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container">
+        © 2015 Copyright Astrid Garth
+        <a class="grey-text text-lighten-4 right" href="#!">Netpoint-Media</a>
+        </div>
+      </div>
+    </footer>
+
     <!--Import jQuery before materialize.js-->
     <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <!-- Compiled and minified JavaScript -->
