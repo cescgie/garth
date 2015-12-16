d9a8432fdc2387b80e27330c74358997c7ef6a07
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-12-16 15:46:30 +0100

    header inside container

--- views/header.php
+++ views/header.php
@@ -15,45 +15,50 @@
 <body>
     <div align="center" class="margin-auto">
      <a href="/" >
-       <img style="width:300px;height:70px;"  class="logo-media" src="<?= URL::ASSETS('black-logo.png','logo') ?>">
+       <img style="width:250px;height:70px;"  class="logo-media" src="<?= URL::ASSETS('black-logo.png','logo') ?>">
      </a>
    </div>
     <nav class="black" role="navigation">
+      <div class="container">
+        <div class="nav-wrapper">
+          <a href="<?php DIR ?>" >
+            <img style="width:250px;height:70px;" class="logo-media-grey hide-on-med-and-down" src="<?= URL::ASSETS('grey-logo.png','logo') ?>">
+          </a>
+         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+         <!-- menu for screen with width above 992px -->
+         <div class="right hide-on-med-and-down shown">
+           <div class="row">
+             <div class="col s12">
+               <ul class="tabs black">
+                 <li class="tab col s3 deskt"><a class="active" href="#portfolio">PORTFOLIO</a></li>
+                 <li class="tab col s3 deskt"><a href="#vita">VITA</a></li>
+                 <li class="tab col s3 deskt"><a href="#referenzen">REFERENZEN</a></li>
+                 <li class="tab col s3 deskt"><a href="#links">LINKS</a></li>
+                 <li class="tab col s3 deskt"><a href="#kontakt">KONTAKT</a></li>
+                 <li class="tab col s3 deskt"><a href="#impressum">IMPRESSUM</a></li>
+               </ul>
+             </div>
+           </div>
+         </div>
+         <!-- menu for screen max-width 992px -->
+         <div class="middle-nav">
+           <div class="row">
+             <div class="col s12">
+               <ul class="tabs black">
+                 <li class="tab col s3"><a class="active" href="#portfolio">PORTFOLIO</a></li>
+                 <li class="tab col s3"><a href="#vita">VITA</a></li>
+                 <li class="tab col s3"><a href="#referenzen">REFERENZEN</a></li>
+                 <li class="tab col s3"><a href="#links">LINKS</a></li>
+                 <li class="tab col s3"><a href="#kontakt">KONTAKT</a></li>
+                 <li class="tab col s3"><a href="#impressum">IMPRESSUM</a></li>
+               </ul>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div> <!-- container -->
+
      <div class="nav-wrapper">
-       <a href="/" >
-         <img style="width:300px;height:70px;" class="logo-media-grey hide-on-med-and-down" src="<?= URL::ASSETS('grey-logo.png','logo') ?>">
-       </a>
-      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-      <!-- menu for screen with width above 992px -->
-      <div class="right hide-on-med-and-down">
-        <div class="row">
-          <div class="col s12">
-            <ul class="tabs black">
-              <li class="tab col s3 deskt"><a class="active" href="#portfolio">PORTFOLIO</a></li>
-              <li class="tab col s3 deskt"><a href="#vita">VITA</a></li>
-              <li class="tab col s3 deskt"><a href="#referenzen">REFERENZEN</a></li>
-              <li class="tab col s3 deskt"><a href="#links">LINKS</a></li>
-              <li class="tab col s3 deskt"><a href="#kontakt">KONTAKT</a></li>
-              <li class="tab col s3 deskt"><a href="#impressum">IMPRESSUM</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <!-- menu for screen max-width 992px -->
-      <div class="middle-nav">
-        <div class="row">
-          <div class="col s12">
-            <ul class="tabs black">
-              <li class="tab col s3"><a class="active" href="#portfolio">PORTFOLIO</a></li>
-              <li class="tab col s3"><a href="#vita">VITA</a></li>
-              <li class="tab col s3"><a href="#referenzen">REFERENZEN</a></li>
-              <li class="tab col s3"><a href="#links">LINKS</a></li>
-              <li class="tab col s3"><a href="#kontakt">KONTAKT</a></li>
-              <li class="tab col s3"><a href="#impressum">IMPRESSUM</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
       <!-- menu for screen max-width 600px -->
       <ul class="side-nav" id="mobile-demo">
         <li><a class="active" href="#portfolio">PORTFOLIO</a></li>
@@ -63,5 +68,5 @@
         <li><a href="#kontakt">KONTAKT</a></li>
         <li><a href="#impressum">IMPRESSUM</a></li>
       </ul>
-     </div>
+    </div>
  </nav>
