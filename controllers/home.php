b197af12229fe6302cf63dd515c1ea67664aac7b
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-12-22 10:32:50 +0100

    new paralax image for home

--- controllers/home.php
+++ controllers/home.php
@@ -12,8 +12,8 @@ class Home extends Controller {
 
       $this->_view->render('header', $data);
       $this->_view->render('home/carousel', $data);
-      //$this->_view->render('home/paralax', $data);
       $this->_view->render('home/line', $data);
+      $this->_view->render('home/paralax', $data);
       //$this->_view->render('home/paralax2', $data);
       $this->_view->render('home/footer');
    }
